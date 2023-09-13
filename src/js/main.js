e4d0ad90db6393307575269519a460afa2b4d881
AlexandrRepkin <arepkin96@mail.ru>
2023-09-13 18:11:48 +0300

    burger menu

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,7 @@ import calc from "./modules/calc";
 import filter from "./modules/filter";
 import pictureSize from "./modules/pictureSize";
 import accordeon from "./modules/accordeon";
+import burger from "./modules/burger";
 
 window.addEventListener('DOMContentLoaded', () => {
     modals();
@@ -23,4 +24,5 @@ window.addEventListener('DOMContentLoaded', () => {
     pictureSize('.sizes-block');
     // accordeon('.accordion-heading', '.accordion-block');
     accordeon('.accordion-heading');
+    burger('.burger-menu', '.burger');
 });
